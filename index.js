ccdb9e5c99fe9d8a971d235dfde5edbd7a11d821
Deyvin Cano <deyvin@Deyvins-MacBook-Air.local>
2022-01-20 14:44:01 -0500

    carData

---
+++ index.js
@@ -0,0 +1,17 @@
+
+// function readCars(){
+//     const fs = require('fs')
+//     const data = fs.readFileSync('data.json', 'utf-8')
+//     return data
+// }
+// console.log(readCars())
+
+
+
+const fs = require('fs')
+fs.readFile('data.json', 'utf-8', (err, data) => {
+    if (err) {
+        console.log(err)
+    }
+    console.log(data)
+})
